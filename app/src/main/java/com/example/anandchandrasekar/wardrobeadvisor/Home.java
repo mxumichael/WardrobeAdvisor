ff4c5c87825fa118673087d40110b22aad14767f
anandchandrasekar <achandr9@ncsu.edu>
2015-11-11 19:48:02 -0500

    merging filters bar

--- app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
+++ app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
@@ -1,29 +1,16 @@
 package com.example.anandchandrasekar.wardrobeadvisor;
 
-import android.app.Activity;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
-import android.nfc.Tag;
-import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 import android.widget.Toast;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 
 public class Home extends AppCompatActivity {
 
@@ -43,6 +30,17 @@ public class Home extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
                 Intent i = new Intent(getApplicationContext(), nfc_test_page.class);
+                startActivity(i);
+            }
+        });
+
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), FilterBarTestActivity.class);
                 startActivity(i);
             }
         });
