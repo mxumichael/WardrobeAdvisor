ed152a8bd2b676918137cbf941281e2d8066eb25
Michael Xu <mxu@ncsu.edu>
2015-11-11 19:42:53 -0500

    fixing up nfc errors from last commit
    Merge remote-tracking branch 'origin/master'
    
    # Conflicts:
    #	app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
    #	app/src/main/res/layout/content_home.xml

--- app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
+++ app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
@@ -27,9 +27,6 @@ import java.util.Arrays;
 
 public class Home extends AppCompatActivity {
 
-    private NfcAdapter mNfcAdapter;
-    public static final String MIME_TEXT_PLAIN = "text/plain";
-    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +35,6 @@ public class Home extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        nfcTest();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -46,22 +42,30 @@ public class Home extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Intent i = new Intent(getApplicationContext(), nfc_test_page.class);
+                startActivity(i);
             }
         });
     }
 
     private void nfcTest() {
+        NfcAdapter mNfcAdapter;
+        String MIME_TEXT_PLAIN = "text/plain";
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         String is_nfc_ok = nfcSupportCheck();
         if (is_nfc_ok.equals("FAIL")){
             return;
         }
+        setContentView(R.layout.nfc_test_page);
         getIntent();
 
     }
 
     private String nfcSupportCheck() {
+        NfcAdapter mNfcAdapter;
+        String MIME_TEXT_PLAIN = "text/plain";
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
         //copied from http://code.tutsplus.com/tutorials/reading-nfc-tags-with-android--mobile-17278
         if (mNfcAdapter == null) {
@@ -79,106 +83,7 @@ public class Home extends AppCompatActivity {
         }
         return "OK";
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
 
-        /**
-         * It's important, that the activity is in the foreground (resumed). Otherwise
-         * an IllegalStateException is thrown.
-         */
-        setupForegroundDispatch(this, mNfcAdapter);
-    }
-
-    @Override
-    protected void onPause() {
-        /**
-         * Call this before onPause, otherwise an IllegalArgumentException is thrown as well.
-         */
-        stopForegroundDispatch(this, mNfcAdapter);
-
-        super.onPause();
-    }
-
-    @Override
-    protected void onNewIntent(Intent intent) {
-        /**
-         * This method gets called, when a new Intent gets associated with the current activity instance.
-         * Instead of creating a new activity, onNewIntent will be called. For more information have a look
-         * at the documentation.
-         *
-         * In our case this method gets called, when the user attaches a Tag to the device.
-         */
-        handleIntent(intent);
-    }
-
-    private void handleIntent(Intent intent) {
-        /*this part gets called whenever an NFC is detected. I think this calls the Reader to actually read the information*/
-        Toast.makeText(this,"handleIntent has been called", Toast.LENGTH_LONG).show();
-
-        String action = intent.getAction();
-        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
-
-            String type = intent.getType();
-            if (MIME_TEXT_PLAIN.equals(type)) {
-
-                Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-                new NdefReaderTask().execute(tag);
-
-            } else {
-               // Log.d(TAG, "Wrong mime type: " + type);
-                CharSequence temp = "Wrong mime type: " + type;
-                Toast.makeText(this,temp, Toast.LENGTH_LONG).show();
-
-            }
-        } else if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)) {
-
-            // In case we would still use the Tech Discovered Intent
-            Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-            String[] techList = tag.getTechList();
-            String searchedTech = Ndef.class.getName();
-
-            for (String tech : techList) {
-                if (searchedTech.equals(tech)) {
-                    new NdefReaderTask().execute(tag);
-                    break;
-                }
-            }
-        }
-    }
-
-    /**
-     * @param activity The corresponding {@link Activity} requesting the foreground dispatch.
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    public static void setupForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
-        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-
-        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
-
-        IntentFilter[] filters = new IntentFilter[1];
-        String[][] techList = new String[][]{};
-
-        // Notice that this is the same filter as in our manifest.
-        filters[0] = new IntentFilter();
-        filters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
-        filters[0].addCategory(Intent.CATEGORY_DEFAULT);
-        try {
-            filters[0].addDataType(MIME_TEXT_PLAIN);
-        } catch (IntentFilter.MalformedMimeTypeException e) {
-            throw new RuntimeException("Check your mime type.");
-        }
-
-        adapter.enableForegroundDispatch(activity, pendingIntent, filters, techList);
-    }
-
-    /**
-     * @param adapter The {@link NfcAdapter} used for the foreground dispatch.
-     */
-    public static void stopForegroundDispatch(final Activity activity, NfcAdapter adapter) {
-        adapter.disableForegroundDispatch(activity);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -201,73 +106,5 @@ public class Home extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    /**
-     * Background task for reading the data. Do not block the UI thread while reading.
-     *
-     * @author Ralf Wondratschek
-     *
-     */
-    private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
-        TextView mTextView;
-        @Override
-        protected String doInBackground(Tag... params) {
-            Tag tag = params[0];
 
-            Ndef ndef = Ndef.get(tag);
-            if (ndef == null) {
-                // NDEF is not supported by this Tag.
-                return null;
-            }
-
-            NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-            NdefRecord[] records = ndefMessage.getRecords();
-            for (NdefRecord ndefRecord : records) {
-                if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-                    try {
-                        return readText(ndefRecord);
-                    } catch (UnsupportedEncodingException e) {
-                        Log.e(ndefRecord.toString(), "Unsupported Encoding", e);
-                    }
-                }
-            }
-
-            return null;
-        }
-
-        private String readText(NdefRecord record) throws UnsupportedEncodingException {
-        /*
-         * See NFC forum specification for "Text Record Type Definition" at 3.2.1
-         *
-         * http://www.nfc-forum.org/specs/
-         *
-         * bit_7 defines encoding
-         * bit_6 reserved for future use, must be 0
-         * bit_5..0 length of IANA language code
-         */
-
-            byte[] payload = record.getPayload();
-
-            // Get the Text Encoding
-            String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-            // Get the Language Code
-            int languageCodeLength = payload[0] & 0063;
-
-            // String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-            // e.g. "en"
-
-            // Get the Text
-            return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            mTextView = (TextView) findViewById(R.id.nfc_test);
-
-            if (result != null) {
-                mTextView.setText("Read content: " + result);
-            }
-        }
-    }
 }
