ac062883ce4fe2bd859a337d8994956d51f447b0
anandchandrasekar <achandr9@ncsu.edu>
2015-11-23 19:42:52 -0500

    scan button changes

--- app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
+++ app/src/main/java/com/example/anandchandrasekar/wardrobeadvisor/Home.java
@@ -2,14 +2,13 @@ package com.example.anandchandrasekar.wardrobeadvisor;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -43,23 +42,11 @@ public class Home extends AppCompatActivity implements FilterKindFragment.Filter
         allFiltersFragment = (AllFiltersFragment) getSupportFragmentManager().findFragmentById(R.id.allFiltersFragment);
         currentlySelectedFilters = new ArrayList<ItemFilter>();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button scanButton = (Button) findViewById(R.id.scanItemButton);
+        scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(), Scan.class);
-                startActivity(i);
-            }
-        });
-
-        FloatingActionButton viewtest = (FloatingActionButton) findViewById(R.id.viewtest);
-        viewtest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent i = new Intent(getApplicationContext(), ViewItemActivity.class);
-                i.putExtra("item_id", 1);
                 startActivity(i);
             }
         });
